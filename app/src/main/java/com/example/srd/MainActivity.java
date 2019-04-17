38f157dc103eeec760d3f49aaa5fe685031228d3
Frank <f.r.a.n.k.c.v.777@gmail.com>
2019-04-17 16:11:08 -0500

    agrego pequenia validacion al login

--- app/src/main/java/com/example/srd/MainActivity.java
+++ app/src/main/java/com/example/srd/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.srd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextNombreUsuario,editTextPassword;
+    Button  buttonIngresa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextNombreUsuario = findViewById(R.id.EditTextNomUsu);
+        editTextPassword = findViewById(R.id.editTextPasword);
+        buttonIngresa = findViewById(R.id.buttonIngresar);
+        buttonIngresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               boolean estado =  validaLogin();
+                if(estado){
+                    Intent intent = new Intent(getApplicationContext(),listaPisosActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+    }
+    private boolean validaLogin() {
+
+        String nomUsu = editTextNombreUsuario.getText().toString();
+        String password = editTextPassword.getText().toString();
+        boolean estado = false ;
+        if(nomUsu.equals("") || password.equals("") ){
+            Toast.makeText(getApplicationContext(),"NO SE PERMITEN CAMPOS VACIOS", Toast.LENGTH_LONG).show();
+            estado = false;
+        }else {
+
+            estado = true;
+        }
+        return estado;
     }
 }
---
+++ app/src/main/java/com/example/srd/listaPisosActivity.java
@@ -0,0 +1,13 @@
+package com.example.srd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listaPisosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pisos);
+    }
+}
